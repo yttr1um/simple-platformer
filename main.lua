116d7b852fd0d7f7e270860e1b22a18c65fa52b5
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-16 15:28:46 +0400

    initial setup

---
+++ main.lua
@@ -0,0 +1,14 @@
+function love.load()
+    SCREEN_WIDTH = 1280
+    SCREEN_HEIGHT = 720
+
+    love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT,  {fullscreen=false, vsync=true})
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+
+end
