76031448dd148d7038e402f85c615580c71ccdb5
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-16 15:30:01 +0400

    Added wall object constructor

--- main.lua
+++ main.lua
@@ -1,3 +1,19 @@
+function newWall(x, y, w, h) 
+    local wall = {
+        x = x,
+        y = y,
+        w = w,
+        h = h,
+        tag = "wall",
+
+        body = love.physics.newBody(world, x, y, "static"),
+        shape = love.physics.newRectangleShape(w/2, h/2, w, h),
+    }
+
+    wall.fixture = love.physics.newFixture(wall.body, wall.shape)
+    walls.insert(walls, wall)
+end
+
 function love.load()
     SCREEN_WIDTH = 1280
     SCREEN_HEIGHT = 720
