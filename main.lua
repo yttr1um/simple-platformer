2b9d946de3d9b3512545803feb81aae806e5e71a
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-16 15:37:11 +0400

    Add border (walls on 4 sides)

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+walls = {}
+
 function newWall(x, y, w, h) 
     local wall = {
         x = x,
@@ -11,7 +13,7 @@ function newWall(x, y, w, h)
     }
 
     wall.fixture = love.physics.newFixture(wall.body, wall.shape)
-    walls.insert(walls, wall)
+    table.insert(walls, wall)
 end
 
 function love.load()
@@ -19,12 +21,26 @@ function love.load()
     SCREEN_HEIGHT = 720
 
     love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT,  {fullscreen=false, vsync=true})
+
+    world = love.physics.newWorld(0, 0)
+
+
+
+    -- walls (border)
+
+    newWall(0, 0, SCREEN_WIDTH, 20) --top
+    newWall(0, 0, 20, SCREEN_HEIGHT) -- left
+    newWall(SCREEN_WIDTH-20, 0, 20, SCREEN_HEIGHT) --right
+    newWall(0, SCREEN_HEIGHT-20, SCREEN_WIDTH, 20) --bottom
 end
 
 function love.update(dt)
     
 end
 
-function love.draw()
-
+function love.draw()    
+    for i = 1, #walls do
+        local w = walls[i]
+        love.graphics.rectangle("line", w.x, w.y, w.w, w.h)
+    end
 end
