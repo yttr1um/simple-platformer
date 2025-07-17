9182cb1b410d31246568ff6eda8e6d878689c8a3
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-17 19:54:32 +0400

    initial setup of spikes

---
+++ spike.lua
@@ -0,0 +1,53 @@
+Spike = {img = love.graphics.newImage("assets/spikes.png")}
+Spike.__index = Spike
+ActiveSpikes = {}
+
+Spike.width = Spike.img:getWidth()
+Spike.height = Spike.img:getHeight()
+
+function Spike.new(x, y)
+    local instance = setmetatable({}, Spike)
+    instance.x = x
+    instance.y = y
+
+    instance.physics = {}
+    instance.physics.body = love.physics.newBody(World, instance.x, instance.y, "static")
+    instance.physics.shape = love.physics.newRectangleShape(instance.width, instance.height)
+    instance.physics.fixture = love.physics.newFixture(instance.physics.body, instance.physics.shape)
+    instance.physics.fixture:setSensor(true)
+
+    instance.sound = love.audio.newSource("sounds/Spike_collect.wav", "static")
+    table.insert(ActiveSpikes, instance)
+end
+
+function Spike:update(dt)
+
+end
+
+function Spike:draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(self.img, self.x, self.y, 0, self.scaleX, 1, self.width/2, self.height/2)
+end
+
+function Spike.updateAll(dt)
+    for i, instance in ipairs(ActiveSpikes) do
+        instance:update(dt)
+    end
+end
+
+function Spike.drawAll()
+    for i, instance in ipairs(ActiveSpikes) do
+        instance:draw()
+    end
+end
+
+function Spike.beginContact(a, b, contact)
+    for i, instance in ipairs(ActiveSpikes) do
+        if a == instance.physics.fixture or b == instance.physics.fixture then
+            if a == Player.physics.fixture or b == Player.physics.fixture then
+                instance.toBeRemoved = true
+                return true
+            end
+        end
+    end
+end
